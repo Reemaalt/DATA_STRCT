6b5d72898fc7a055ef6cfbb5aa6cd8914eb5e002
Yaraalfouzan <alfouzan.yara@gmail.com>
2023-10-03 15:24:11 +0300

    Update Node.java

--- Phase1/Node.java
+++ Phase1/Node.java
@@ -2,7 +2,8 @@ public class Node<T> {
         public T data; 
         public Node<T> next;
 public Node () { 
-    data = null; next = null;
+    data = null; 
+    next = null;
 }
 public Node (T val) {
     data = val;
