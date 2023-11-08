33c0383dfa1fda9157c94dd0d53fd6dc49e38a9f
Yaraalfouzan <alfouzan.yara@gmail.com>
2023-11-08 20:49:10 +0300

    add and find

--- phase2/ContactBST.java
+++ phase2/ContactBST.java
@@ -1,5 +1,7 @@
 package phase2;
 
+import javax.print.DocFlavor.STRING;
+
 public class ContactBST <T extends Comparable<T>> {
     private  Node<T> root , current;
     private T data;
@@ -37,32 +39,56 @@ private T searchKeyRecursive(Node<T> data, String key) {
     return searchKeyRecursive(data.right, key);
 }
 
-private boolean addcontact (Node<T> contact, T val){
-    ContactBST<T> p;
+public boolean findkey(String name) { 
+    Node<T> p = root ,q = root;
+    if(isEmpty())
+    return false;
+
+    while(p != null) { 
+        q = p;
+    if(p.key.equals(name)) 
+    { 
+    current = p;
+    return true;
+    }
+    else if(name.compareTo(p.key)< 0)
+    p=p.left;
+    else 
+    p=p.right;
+    }
+    
+    current = q; 
+    return false;
+}
+
+private boolean addcontact ( <Contact> contact, String name){
+    Node<T> p;
     Node<T> q=current;
-    //also usse order traversal for phone num
-    if (findkey (contact.key)|| recInOrderTraversal(contact , contact.data).equals(contact.data.phoneNumber))
+
+    //is rec in order used correctly?
+    if (findkey (contact.getName())|| recInOrderTraversal(contact , contact.getPhoneNumber()))
     {  
        current=q;
        return false;
     }
-    p=new ContactBST<T>(name,val);
+    
+    p=new Node<T>(name, contact);
     if (isEmpty()){
         root=current=p;
         return true;
     }
     else{
         //current on parent of key
-        if(name.compareTo(current.key)<0)//how does compareto work
-        current.left=p;
+        if(name.compareTo(current.key)<0)//to insert in alphabatecal order
+              current.left=p;
         else
-        current.right=p;
+              current.right=p;
         current=p;
         return true;
     }
     
 }
-}
+
 
 public String toString() {
     String str = "";
@@ -72,7 +98,7 @@ public String toString() {
     return str;
 
 }
-
+//how does it work
 private String recInOrderTraversal(Node<T> node, String result) {
     if (!isEmpty()) {
         result = recInOrderTraversal(node.left, result);
