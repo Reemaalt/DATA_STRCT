e62233a6654f74fe6e2a306c59151f60bc2c29c8
Reemaalt <reema.altj@gmail.com>
2023-12-01 21:12:27 +0300

    nothing

--- phase2/Node.java
+++ phase2/Node.java
@@ -4,22 +4,31 @@ public class Node<T> {
         public T data; 
         public Node<T> left , right;
 
-public Node () { 
-    left = right = null ;
+
+
+        
+    public Node(String key, T data) {
+        this.key = key;
+        this.data = data;
+        this.left = null;
+        this.right = null;
     }
+// public Node () { 
+//     left = right = null ;
+//     }
 
-public Node (String key ,T data) {
-    this.key = key  ;  
-    this.data = data;
-    left = right = null;
-} 
+// public Node (String key ,T data) {
+//     this.key = key  ;  
+//     this.data = data;
+//     left = right = null;
+// } 
 
-public Node(String key, T data, Node<T> left, Node<T> right){
-    this.key = key  ;  
-    this.data = data;
-    this.left = left;
-    this.right = right ;
-}
+// public Node(String key, T data, Node<T> left, Node<T> right){
+//     this.key = key  ;  
+//     this.data = data;
+//     this.left = left;
+//     this.right = right ;
+// }
 
 public T getData() {
         return data;
